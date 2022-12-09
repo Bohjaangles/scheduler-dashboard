8c61a8b13580519be4ef1e91fc071df8d62dc347
Russel Mueller <bohjaangles@Russels-MacBook-Pro.local>
2022-12-08 18:44:49 -0600

    adding a Panels component, and importing it into Dahboard, mapping over mock data for render panels correctly

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,36 @@ import React, { Component } from "react";
 
 import Loading from "./Loading";
 import classnames from "classnames";
+import Panel from "./Panel";
 
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 class Dashboard extends Component {
   state = {
     loading: true,
-  }
+    focused: null,
+  };
   render() {
     const dashboardClasses = classnames("dashboard");
 
@@ -14,10 +39,21 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    return (
-    <main className={dashboardClasses}>
+    const panels = data.map(panel => {
+      return (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      );
+    });
 
-    </main>
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
     );
   }
 }
