24879460ab4bbaf1256d4d6d5e2d1d6e0bbad727
Russel Mueller <bohjaangles@Russels-MacBook-Pro.local>
2022-12-08 21:04:46 -0600

    adding websockets and axios connection to api for real time data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,61 +1,127 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import Loading from "./Loading";
 import classnames from "classnames";
 import Panel from "./Panel";
+import { setInterview } from "helpers/reducers";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 class Dashboard extends Component {
+
   state = {
-    loading: true,
+    loading: false,
     focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
   };
-  render() {
-    const dashboardClasses = classnames("dashboard");
 
-    if (this.state.loading) {
-      return <Loading />;
-    }
-
-    const panels = data.map(panel => {
-      return (
-        <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
-        />
-      );
-    });
-
-    return (
-      <main className={dashboardClasses}>
-        {panels}
-      </main>
-    );
+  selectPanel(id) {
+    this.setState(this.state.focused ? { focused: null } : { focused: id });
   }
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    Promise.all([
+      axios.get("/api/days"),
+        axios.get("/api/appointments"),
+        axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+          this.setState({
+            loading: false,
+            days: days.data,
+            appointments: appointments.data,
+            interviewers: interviewers.data
+          });
+        });
+    
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  } 
+
+// selectPanel(id) {           <== Compass suggested this method for select panel
+//   this.setState(previousState => ({
+//     focused: previousState.focused !== null ? null : id
+//   }));
+// }
+
+render() {
+  const dashboardClasses = classnames('dashboard', {
+    "dashboard--focused": this.state.focused
+  });
+
+  if (this.state.loading) {
+    return <Loading />;
+  }
+
+  const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => {
+    return (
+      <Panel
+        key={panel.id}
+        label={panel.label}
+        value={panel.getValue(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
+      />
+    );
+  });
+
+  return (
+    <main className={dashboardClasses}>
+      {panels}
+    </main>
+  );
+}
 }
 
 export default Dashboard;
